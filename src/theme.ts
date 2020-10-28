e2fd0359bbd24f8fbfc299e7bd403987d53f2b9d
Ben Walker <bwalke05@gmail.com>
2020-10-27 20:15:37 -0400

    Stub out default/dark themes (#3)

--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,14 @@ import { Theme } from "theme-ui";
 
 export const theme: Theme = {
   useColorSchemeMediaQuery: true,
+  colors: {
+    background: "white",
+    text: "black",
+    modes: {
+      dark: {
+        background: "black",
+        text: "white",
+      },
+    },
+  },
 };
