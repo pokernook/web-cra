e55af6b63ad23cbe53531cec6bcadc99c67bcb83
Ben Walker <bwalke05@gmail.com>
2020-10-28 20:04:39 -0400

    Remove `code` component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,6 @@ const Home = (): JSX.Element => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
