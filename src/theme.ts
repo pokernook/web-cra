c128f23bd27efe02a001db2fc88bac5d7bbac56f
Ben Walker <bwalke05@gmail.com>
2020-11-02 19:43:12 -0500

    Adjust theme for inputs

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,7 @@ import { Theme } from "theme-ui";
 
 export const theme: Theme = {
   useColorSchemeMediaQuery: true,
+  useLocalStorage: true,
   colors: {
     background: "#fff",
     text: "#000",
@@ -18,13 +19,16 @@ export const theme: Theme = {
       "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
     heading: "inherit",
   },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
   fontWeights: {
     body: 400,
     heading: 700,
+    bold: 700,
   },
   styles: {
     root: {
       fontFamily: "body",
+      fontSize: 1,
       padding: 0,
       margin: 0,
     },
@@ -43,7 +47,10 @@ export const theme: Theme = {
       fontWeight: "bold",
     },
     input: {
-      mt: 1,
+      mt: "0.375rem",
+      p: 1,
+      bg: "#fafbfc",
+      borderColor: "#e1e4e8",
     },
   },
 };
