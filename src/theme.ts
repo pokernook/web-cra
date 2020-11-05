94642709c399b6c8e836edc3812145cc467008c7
Ben Walker <bwalke05@gmail.com>
2020-11-04 21:29:18 -0500

    Theme ui experiments (#11)
    * Add heading font theming
    
    * Switch to theme ui heading
    
    * Wrap in empty component
    
    * Turn off prop-types rule
    
    * Add theme decorator to storybook
    
    * Update dependencies
    
    * Add buttons key
    
    * Add button component
    
    * Use storybook dark theme
    
    * Lighten dark mode background
    
    * Fill out README
    
    * Reformat heading
    
    * Resintall because storybook is terrible
    
    * Add styling to primary button
    
    * Use theme ui button directly
    
    * Switch to storybook light mode
    
    * Add a Field story and theming
    
    * Update theme
    
    * Adjust theme for inputs
    
    * NPM shenanigans
    
    * Add card styling
    
    * Update dependencies
    
    * Remove margin from theme
    
    * Darken primary color
    
    * Add another font size
    
    * Remove field story
    
    * Setup primary button
    
    * Add input story

--- src/theme.ts
+++ src/theme.ts
@@ -2,25 +2,57 @@ import { Theme } from "theme-ui";
 
 export const theme: Theme = {
   useColorSchemeMediaQuery: true,
+  useLocalStorage: true,
   colors: {
     background: "#fff",
     text: "#000",
-    modes: {
-      dark: {
-        background: "#0a0a0a",
-        text: "#fff",
-      },
-    },
+    primary: "#2ea44f",
   },
   fonts: {
     body:
       "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+    heading: "inherit",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 600,
   },
   styles: {
     root: {
       fontFamily: "body",
+      fontSize: 1,
       padding: 0,
       margin: 0,
+    },
+  },
+  buttons: {
+    primary: {
+      color: "background",
+      bg: "primary",
+      px: 3,
+      py: 2,
+    },
+  },
+  cards: {
+    primary: {
+      padding: 3,
+      borderRadius: 4,
+      border: "solid",
+      borderWidth: 1,
+      borderColor: "#e1e4e8",
+    },
+  },
+  forms: {
+    label: {
+      fontSize: 1,
+      fontWeight: "bold",
+    },
+    input: {
+      p: 1,
+      bg: "#fafbfc",
+      borderColor: "#e1e4e8",
     },
   },
 };
