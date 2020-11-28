de2fc9867996e3fcf4f5fbd0d8785780601cbb85
Lint Action <lint-action@samuelmeuli.com>
2020-11-28 01:33:00 +0000

    Fix code style issues with Prettier

--- src/@types/graphql.d.ts
+++ src/@types/graphql.d.ts
@@ -1,8 +1,12 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -13,63 +17,59 @@ export type Scalars = {
 };
 
 export type AuthPayload = {
-  __typename?: 'AuthPayload';
-  token?: Maybe<Scalars['String']>;
+  __typename?: "AuthPayload";
+  token?: Maybe<Scalars["String"]>;
   user?: Maybe<User>;
 };
 
 export type User = {
-  __typename?: 'User';
-  id: Scalars['String'];
-  email: Scalars['String'];
-  username: Scalars['String'];
-  discriminator: Scalars['Int'];
+  __typename?: "User";
+  id: Scalars["String"];
+  email: Scalars["String"];
+  username: Scalars["String"];
+  discriminator: Scalars["Int"];
 };
 
 export type UserWhereUniqueInput = {
-  id?: Maybe<Scalars['String']>;
-  email?: Maybe<Scalars['String']>;
+  id?: Maybe<Scalars["String"]>;
+  email?: Maybe<Scalars["String"]>;
   UserTag?: Maybe<UserTagCompoundUniqueInput>;
 };
 
 export type UserTagCompoundUniqueInput = {
-  username: Scalars['String'];
-  discriminator: Scalars['Int'];
+  username: Scalars["String"];
+  discriminator: Scalars["Int"];
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   user?: Maybe<User>;
   users: Array<User>;
 };
-
 
 export type QueryUserArgs = {
   where: UserWhereUniqueInput;
 };
 
-
 export type QueryUsersArgs = {
-  take?: Maybe<Scalars['Int']>;
-  skip?: Maybe<Scalars['Int']>;
+  take?: Maybe<Scalars["Int"]>;
+  skip?: Maybe<Scalars["Int"]>;
   cursor?: Maybe<UserWhereUniqueInput>;
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   signUp?: Maybe<AuthPayload>;
   logIn?: Maybe<AuthPayload>;
 };
 
-
 export type MutationSignUpArgs = {
-  email: Scalars['String'];
-  username: Scalars['String'];
-  password: Scalars['String'];
+  email: Scalars["String"];
+  username: Scalars["String"];
+  password: Scalars["String"];
 };
 
-
 export type MutationLogInArgs = {
-  email: Scalars['String'];
-  password: Scalars['String'];
+  email: Scalars["String"];
+  password: Scalars["String"];
 };
