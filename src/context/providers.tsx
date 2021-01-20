734b875711ca9cd173c55ada7c1d0bbf42d57c17
Ben Walker <bwalke05@gmail.com>
2021-01-19 19:04:55 -0500

    Add providers component

---
+++ src/context/providers.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "theme-ui";
+import { Provider as UrqlProvider } from "urql";
+
+import { client } from "../graphql";
+import { theme } from "../theme";
+
+export const Providers: FC = ({ children }) => (
+  <React.StrictMode>
+    <UrqlProvider value={client}>
+      <ThemeProvider theme={theme}>
+        <Router>{children}</Router>
+      </ThemeProvider>
+    </UrqlProvider>
+  </React.StrictMode>
+);
