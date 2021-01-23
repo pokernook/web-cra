22b7981f4e20f6f68acdef6d35838062d97bbaa0
Ben Walker <bwalke05@gmail.com>
2021-01-23 17:28:06 -0500

    Remove user details

--- src/routes/PrivateApp.tsx
+++ src/routes/PrivateApp.tsx
@@ -1,26 +1,14 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import { Helm } from "../layouts/Helm";
-import { useUserStore } from "../stores/user";
 
-export const PrivateApp = () => {
-  const user = useUserStore((state) => state.user);
-
-  return (
-    <Helm>
-      <Switch>
-        <Route exact path="/">
-          <pre>ID: {user?.id}</pre>
-          <pre>Created: {user?.createdAt}</pre>
-          <pre>Email: {user?.email}</pre>
-          <pre>
-            Tag: {user?.username}#{user?.discriminator}
-          </pre>
-        </Route>
-        <Route>
-          <Redirect to="/" />
-        </Route>
-      </Switch>
-    </Helm>
-  );
-};
+export const PrivateApp = () => (
+  <Helm>
+    <Switch>
+      <Route exact path="/"></Route>
+      <Route>
+        <Redirect to="/" />
+      </Route>
+    </Switch>
+  </Helm>
+);
