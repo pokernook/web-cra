45c526ae6278212f3de45857548bd0f70dfc2f85
Ben Walker <bwalke05@gmail.com>
2021-01-23 18:11:21 -0500

    Split layout into multiple components

--- src/components/layouts/Helm.tsx
+++ src/components/layouts/Helm.tsx
@@ -1,22 +1,41 @@
 /** @jsxImportSource theme-ui */
 import { FC } from "react";
 
+const Sidebar = () => (
+  <aside
+    sx={{
+      bg: "muted",
+      borderRadius: 0,
+      borderRight: "solid",
+      borderRightColor: "border",
+      borderRightWidth: 1,
+      flexGrow: 1,
+      flexBasis: "sidebar",
+      minHeight: "100vh",
+    }}
+  ></aside>
+);
+
+const Header = () => <header sx={{ width: "100%" }}></header>;
+
+const Footer = () => <footer sx={{ width: "100%" }}></footer>;
+
 export const Helm: FC = ({ children }) => (
   <div sx={{ display: "flex", flexWrap: "wrap" }}>
-    <aside
+    <Sidebar />
+    <div
       sx={{
-        bg: "muted",
-        borderRadius: 0,
-        borderRight: "solid",
-        borderRightColor: "border",
-        borderRightWidth: 1,
-        flexGrow: 1,
-        flexBasis: "sidebar",
+        display: "inherit",
+        flexDirection: "column",
+        flexGrow: 99999,
+        flexBasis: 0,
         minHeight: "100vh",
+        minWidth: 320,
       }}
-    ></aside>
-    <main sx={{ flexGrow: 99999, flexBasis: 0, minWidth: 320 }}>
-      {children}
-    </main>
+    >
+      <Header />
+      <main sx={{ flex: "1 1 auto", width: "100%" }}>{children}</main>
+      <Footer />
+    </div>
   </div>
 );
