ed3bccf836a1bb5e4e7f6b50782720c22c6cc185
Ben Walker <bwalke05@gmail.com>
2021-01-25 22:35:32 -0500

    Add a route to the profile

--- src/components/routes/PrivateApp.tsx
+++ src/components/routes/PrivateApp.tsx
@@ -1,10 +1,14 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import { Helm } from "../layouts/Helm";
+import { Profile } from "../pages/Profile";
 
 export const PrivateApp = () => (
   <Helm>
     <Switch>
+      <Route exact path="/profile">
+        <Profile />
+      </Route>
       <Route exact path="/"></Route>
       <Route>
         <Redirect to="/" />
