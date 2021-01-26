78e374692a6ba06bcae5d10da96792edf6e95ca1
Ben Walker <bwalke05@gmail.com>
2021-01-25 19:55:53 -0500

    Add nav link style

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -40,7 +40,7 @@ export const theme: Theme = {
     },
   },
   sizes: {
-    sidebar: 320,
+    sidebar: 280,
   },
   buttons: {
     primary: {
@@ -49,6 +49,25 @@ export const theme: Theme = {
       px: 3,
       py: 2,
       "&:hover": {
+        cursor: "pointer",
+      },
+    },
+  },
+  links: {
+    nav: {
+      alignItems: "center",
+      border: "solid",
+      borderColor: "background",
+      borderRadius: 4,
+      borderWidth: 1,
+      color: "text",
+      display: "flex",
+      minHeight: 50,
+      p: 2,
+      textDecoration: "none",
+      ":hover": {
+        bg: alpha("link", 0.2),
+        borderColor: "link",
         cursor: "pointer",
       },
     },
