03b40b962e5c7c130797725c3a900f4ea8c39d43
Ben Walker <bwalke05@gmail.com>
2021-01-26 23:14:15 -0500

    Remove stub profile

--- src/components/routes/PrivateApp.tsx
+++ src/components/routes/PrivateApp.tsx
@@ -1,14 +1,11 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import { Helm } from "../layouts/Helm";
-import { Profile } from "../pages/Profile";
 
 export const PrivateApp = () => (
   <Helm>
     <Switch>
-      <Route exact path="/profile">
-        <Profile />
-      </Route>
+      <Route exact path="/settings"></Route>
       <Route exact path="/"></Route>
       <Route>
         <Redirect to="/" />
