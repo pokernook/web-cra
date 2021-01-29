9a16eb8e9ab0de71714d90d97bb4ec47ba8f4e61
Ben Walker <bwalke05@gmail.com>
2021-01-28 22:42:31 -0500

    Add a header to the settings page

--- src/components/routes/PrivateApp/Settings.tsx
+++ src/components/routes/PrivateApp/Settings.tsx
@@ -1,10 +1,28 @@
 import { FC } from "react";
 import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
-import { Container } from "theme-ui";
+import { Avatar, Container, Flex, Heading } from "theme-ui";
 
-const SettingsLayout: FC = ({ children }) => (
-  <Container sx={{ maxWidth: 980, pt: 20 }}>{children}</Container>
-);
+import { useUserStore } from "../../../stores/user";
+
+const SettingsLayout: FC = ({ children }) => {
+  const [user, getAvatar] = useUserStore((state) => [
+    state.user,
+    state.getAvatar,
+  ]);
+
+  return (
+    <Container sx={{ maxWidth: 980, pt: 20 }}>
+      <Flex>
+        <Avatar src={getAvatar()} sx={{ width: 64, height: 64, mr: 3 }} />
+        <Heading>{user?.username}</Heading>
+        <Heading sx={{ color: "mutedText", fontWeight: "body" }}>
+          #{user?.discriminator?.toString().padStart(4, "0")}
+        </Heading>
+      </Flex>
+      {children}
+    </Container>
+  );
+};
 
 export const Settings = () => {
   const { path } = useRouteMatch();
