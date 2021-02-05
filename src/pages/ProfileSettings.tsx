266ab82a1c269cd6405d8929faad66afb1213469
Ben Walker <bwalke05@gmail.com>
2021-02-05 17:23:59 -0500

    Add username and profile picture forms

--- src/pages/ProfileSettings.tsx
+++ src/pages/ProfileSettings.tsx
@@ -1,23 +1,65 @@
-import { Button, Divider, Field, Heading } from "theme-ui";
+/** @jsxImportSource theme-ui */
+import { useForm } from "react-hook-form";
+import { Avatar, Box, Button, Divider, Field, Heading } from "theme-ui";
 
+import {
+  UpdateUsernameMutationVariables,
+  useUpdateUsernameMutation,
+} from "../graphql/types";
 import { useUserStore } from "../stores/user";
 
-export const ProfileSettings = () => {
-  const [user] = useUserStore((state) => [state.user]);
+const UpdateUsernameForm = () => {
+  const user = useUserStore((state) => state.user);
+  const { register, handleSubmit } = useForm<UpdateUsernameMutationVariables>();
+  const [, updateUsername] = useUpdateUsernameMutation();
+
+  const onSubmit = handleSubmit((data) => updateUsername(data));
 
   return (
     <>
-      <Heading as="h1">Profile</Heading>
+      <Heading as="h1">Username</Heading>
       <Divider sx={{ my: 3 }} />
-      <Field
-        label="Username"
-        name="username"
-        type="text"
-        defaultValue={user?.username}
-        spellCheck={false}
-        mb={3}
-      />
-      <Button variant="primary">Save username</Button>
+      <Box as="form" onSubmit={onSubmit}>
+        <Field
+          defaultValue={user?.username}
+          label="Username"
+          name="newUsername"
+          type="text"
+          ref={register({ required: true })}
+          spellCheck={false}
+          sx={{ mb: 3 }}
+        />
+        <Button variant="primary" type="submit" sx={{ mb: 4 }}>
+          Save username
+        </Button>
+      </Box>
     </>
   );
 };
+
+const UpdateProfilePictureForm = () => {
+  const getAvatar = useUserStore((state) => state.getAvatar);
+
+  return (
+    <>
+      <Heading as="h1">Profile picture</Heading>
+      <Divider sx={{ my: 3 }} />
+      <Box sx={{ position: "relative" }}>
+        <Avatar src={getAvatar()} sx={{ width: 200, height: 200 }} />
+        <Button
+          variant="primary"
+          sx={{ position: "absolute", bottom: 0, left: 0 }}
+        >
+          Edit picture
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export const ProfileSettings = () => (
+  <>
+    <UpdateUsernameForm />
+    <UpdateProfilePictureForm />
+  </>
+);
