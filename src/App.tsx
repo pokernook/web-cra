7f2efe73f5ebef8655cdb0cec13a2be2f0b792e2
Ben Walker <bwalke05@gmail.com>
2021-02-04 23:19:25 -0500

    Use urql hooks to check session

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import { useEffect } from "react";
 
-import { Loading } from ".//pages/Loading";
+import { useMeQuery } from "./graphql/types";
+import { Loading } from "./pages/Loading";
 import { PrivateApp } from "./PrivateApp";
 import { PublicApp } from "./PublicApp";
 import { useUserStore } from "./stores/user";
 
-useUserStore.setState({ fetchingSession: true });
-
 export const App = () => {
-  const [user, checkSession, fetchingSession] = useUserStore((state) => [
-    state.user,
-    state.checkSession,
-    state.fetchingSession,
-  ]);
+  const [session] = useMeQuery();
+  const [user, setUser] = useUserStore((state) => [state.user, state.setUser]);
+
+  const { data } = session;
+  const initialLoad = user === undefined;
 
   useEffect(() => {
     setTimeout(() => {
-      checkSession();
+      setUser(data?.me);
     }, 1000);
-  }, [checkSession]);
+  }, [data, setUser]);
 
-  return fetchingSession ? <Loading /> : user ? <PrivateApp /> : <PublicApp />;
+  return initialLoad ? <Loading /> : user ? <PrivateApp /> : <PublicApp />;
 };
