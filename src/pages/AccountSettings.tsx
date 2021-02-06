582aa533541a15e2b8a976663a2d8b99146e9209
Ben Walker <bwalke05@gmail.com>
2021-02-06 12:10:13 -0500

    Allow account delete

--- src/pages/AccountSettings.tsx
+++ src/pages/AccountSettings.tsx
@@ -1,17 +1,31 @@
 import { Box, Button, Divider, Heading, Text } from "theme-ui";
 
-const DeleteAccountForm = () => (
-  <>
-    <Heading as="h1" color="error">
-      Delete account
-    </Heading>
-    <Divider my={3} />
-    <Box mb={2}>
-      <Text>Be careful, there's no coming back.</Text>
-    </Box>
-    <Button variant="danger">Delete account</Button>
-  </>
-);
+import { useDeleteAccountMutation, useMeQuery } from "../graphql";
+
+const DeleteAccountForm = () => {
+  const [, reexecuteMeQuery] = useMeQuery();
+  const [, deleteAccount] = useDeleteAccountMutation();
+
+  const handleDeleteAccount = async () => {
+    await deleteAccount();
+    reexecuteMeQuery({ requestPolicy: "network-only" });
+  };
+
+  return (
+    <>
+      <Heading as="h1" color="error">
+        Delete account
+      </Heading>
+      <Divider my={3} />
+      <Box mb={2}>
+        <Text>Be careful, there's no coming back.</Text>
+      </Box>
+      <Button variant="danger" onClick={handleDeleteAccount}>
+        Delete account
+      </Button>
+    </>
+  );
+};
 
 export const AccountSettings = () => (
   <>
