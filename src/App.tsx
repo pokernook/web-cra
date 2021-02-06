eee9d427184e3b42771502d4c7bf0d3124dd3903
Ben Walker <bwalke05@gmail.com>
2021-02-05 22:03:06 -0500

    Convert App to urql state

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import { useMeQuery } from "./graphql/types";
 import { Loading } from "./pages/Loading";
 import { PrivateApp } from "./PrivateApp";
 import { PublicApp } from "./PublicApp";
-import { useUserStore } from "./stores/user";
 
 export const App = () => {
-  const [launching, setLaunching] = useState(true);
   const [meQuery] = useMeQuery();
-  const [user, setUser] = useUserStore((state) => [state.user, state.setUser]);
+  const [launching, setLaunching] = useState(true);
 
   const { data, fetching: fetchingUser } = meQuery;
 
@@ -20,14 +18,9 @@ export const App = () => {
     }, 1000);
   });
 
-  // Refresh user data
-  useEffect(() => {
-    setUser(data?.me);
-  }, [data, setUser]);
-
   return fetchingUser || launching ? (
     <Loading />
-  ) : user ? (
+  ) : data?.me ? (
     <PrivateApp />
   ) : (
     <PublicApp />
