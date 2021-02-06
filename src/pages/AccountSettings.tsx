1cc1784c03ab47a5cc847942aac38859e779acba
Ben Walker <bwalke05@gmail.com>
2021-02-06 12:17:13 -0500

    Move password updates to account settings

--- src/pages/AccountSettings.tsx
+++ src/pages/AccountSettings.tsx
@@ -1,6 +1,71 @@
-import { Box, Button, Divider, Heading, Text } from "theme-ui";
+import { useForm } from "react-hook-form";
+import { Box, Button, Divider, Field, Heading, Text } from "theme-ui";
 
-import { useDeleteAccountMutation, useMeQuery } from "../graphql";
+import {
+  UpdatePasswordMutationVariables,
+  useDeleteAccountMutation,
+  useMeQuery,
+  useUpdatePasswordMutation,
+} from "../graphql";
+
+const UpdatePasswordForm = () => {
+  const [, updatePassword] = useUpdatePasswordMutation();
+  const { errors, getValues, handleSubmit, register, reset } = useForm<
+    UpdatePasswordMutationVariables & { confirmPassword: string }
+  >();
+
+  const onSubmit = handleSubmit(async (data) => {
+    const result = await updatePassword(data);
+    if (!result.error) {
+      reset();
+    }
+  });
+
+  return (
+    <>
+      <Heading as="h1">Password</Heading>
+      <Divider my={3} />
+      <Box as="form" onSubmit={onSubmit}>
+        <Field
+          label="Old password"
+          name="oldPassword"
+          type="password"
+          ref={register({ required: true })}
+          mb={3}
+        />
+
+        <Field
+          label="New password"
+          name="newPassword"
+          type="password"
+          ref={register({ required: true })}
+          mb={3}
+        />
+
+        <Field
+          label="Confirm new password"
+          name="confirmPassword"
+          type="password"
+          ref={register({
+            required: true,
+            validate: {
+              passwordMatch: (value) =>
+                value === getValues().newPassword ||
+                "Password confirmation doesn't match the password",
+            },
+          })}
+        />
+        <Box mb={3} mt={1}>
+          <Text variant="error">{errors.confirmPassword?.message}</Text>
+        </Box>
+
+        <Button variant="secondary" type="submit" mb={4}>
+          Update password
+        </Button>
+      </Box>
+    </>
+  );
+};
 
 const DeleteAccountForm = () => {
   const [, reexecuteMeQuery] = useMeQuery();
@@ -29,6 +94,7 @@ const DeleteAccountForm = () => {
 
 export const AccountSettings = () => (
   <>
+    <UpdatePasswordForm />
     <DeleteAccountForm />
   </>
 );
--- src/pages/ProfileSettings.tsx
+++ src/pages/ProfileSettings.tsx
@@ -1,11 +1,9 @@
 import { useForm } from "react-hook-form";
-import { Avatar, Box, Button, Divider, Field, Heading, Text } from "theme-ui";
+import { Avatar, Box, Button, Divider, Field, Heading } from "theme-ui";
 
 import {
-  UpdatePasswordMutationVariables,
   UpdateUsernameMutationVariables,
   useMeQuery,
-  useUpdatePasswordMutation,
   useUpdateUsernameMutation,
 } from "../graphql";
 import { generateAvatarSvg } from "../util/generate-avatar";
@@ -65,69 +63,9 @@ const UpdateProfilePictureForm = () => {
   );
 };
 
-const UpdatePasswordForm = () => {
-  const [, updatePassword] = useUpdatePasswordMutation();
-  const { errors, getValues, handleSubmit, register, reset } = useForm<
-    UpdatePasswordMutationVariables & { confirmPassword: string }
-  >();
-
-  const onSubmit = handleSubmit(async (data) => {
-    const result = await updatePassword(data);
-    if (!result.error) {
-      reset();
-    }
-  });
-
-  return (
-    <>
-      <Heading as="h1">Password</Heading>
-      <Divider my={3} />
-      <Box as="form" onSubmit={onSubmit}>
-        <Field
-          label="Old password"
-          name="oldPassword"
-          type="password"
-          ref={register({ required: true })}
-          mb={3}
-        />
-
-        <Field
-          label="New password"
-          name="newPassword"
-          type="password"
-          ref={register({ required: true })}
-          mb={3}
-        />
-
-        <Field
-          label="Confirm new password"
-          name="confirmPassword"
-          type="password"
-          ref={register({
-            required: true,
-            validate: {
-              passwordMatch: (value) =>
-                value === getValues().newPassword ||
-                "Password confirmation doesn't match the password",
-            },
-          })}
-        />
-        <Box mb={3} mt={1}>
-          <Text variant="error">{errors.confirmPassword?.message}</Text>
-        </Box>
-
-        <Button variant="secondary" type="submit" mb={4}>
-          Update password
-        </Button>
-      </Box>
-    </>
-  );
-};
-
 export const ProfileSettings = () => (
   <>
     <UpdateUsernameForm />
     <UpdateProfilePictureForm />
-    <UpdatePasswordForm />
   </>
 );
