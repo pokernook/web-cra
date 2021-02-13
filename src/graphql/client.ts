fcb5e70656f7eb4a5cf4426c27ffb5aa5d6f9661
Ben Walker <bwalke05@gmail.com>
2021-02-13 10:46:16 -0500

    Use the default cache

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,17 +1,14 @@
-import { cacheExchange, ResolverConfig } from "@urql/exchange-graphcache";
-import { createClient, dedupExchange, fetchExchange } from "urql";
+import {
+  cacheExchange,
+  createClient,
+  dedupExchange,
+  fetchExchange,
+} from "urql";
 
 import { config } from "../config";
 
-const resolvers: ResolverConfig = {
-  User: {
-    discriminator: (parent) =>
-      parent.discriminator?.toString().padStart(4, "0"),
-  },
-};
-
 export const client = createClient({
-  exchanges: [cacheExchange({ resolvers }), dedupExchange, fetchExchange],
+  exchanges: [cacheExchange, dedupExchange, fetchExchange],
   fetchOptions: { credentials: "include" },
   requestPolicy: "cache-and-network",
   url: config.graphqlUrl,
