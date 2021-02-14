986e2fe8f6983eeef8ff2af1e89a3bc53b6b77d6
Ben Walker <bwalke05@gmail.com>
2021-02-14 12:37:56 -0500

    Setup cache exchange

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,15 +1,23 @@
 import { devtoolsExchange } from "@urql/devtools";
-import {
-  cacheExchange,
-  createClient,
-  dedupExchange,
-  fetchExchange,
-} from "urql";
+import { cacheExchange, ResolverConfig } from "@urql/exchange-graphcache";
+import { createClient, dedupExchange, fetchExchange } from "urql";
 
 import { config } from "../config";
 
+const resolvers: ResolverConfig = {
+  User: {
+    discriminator: (parent) =>
+      parent.discriminator?.toString().padStart(4, "0"),
+  },
+};
+
 export const client = createClient({
-  exchanges: [devtoolsExchange, cacheExchange, dedupExchange, fetchExchange],
+  exchanges: [
+    devtoolsExchange,
+    cacheExchange({ resolvers }),
+    dedupExchange,
+    fetchExchange,
+  ],
   fetchOptions: { credentials: "include" },
   requestPolicy: "cache-and-network",
   url: config.graphqlUrl,
