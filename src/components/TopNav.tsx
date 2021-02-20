81cd924aaa98599540d9fc91376e287d7fff6927
Ben Walker <bwalke05@gmail.com>
2021-02-20 18:18:50 -0500

    Add TopNav component

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,18 @@
+/** @jsxImportSource theme-ui */
+
+export const TopNav = () => (
+  <header
+    sx={{
+      alignItems: "center",
+      justifyContent: "center",
+      bg: "muted",
+      display: "flex",
+      position: "sticky",
+      top: 0,
+      borderBottom: "solid",
+      borderBottomColor: "border",
+      borderBottomWidth: 1,
+      zIndex: 1,
+    }}
+  ></header>
+);
