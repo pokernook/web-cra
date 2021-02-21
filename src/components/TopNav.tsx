91882bca15fe54688cd3fc650ff532e8c75a734e
Ben Walker <bwalke05@gmail.com>
2021-02-20 21:34:59 -0500

    Add user avatar to top nav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,26 +1,42 @@
 /** @jsxImportSource theme-ui */
 import { NavLink } from "react-router-dom";
-import { Image } from "theme-ui";
+import { Flex, Image } from "theme-ui";
 
 import logo from "../assets/logo.svg";
+import { UserAvatar } from "../components/UserAvatar";
+import { useMeQuery } from "../graphql";
 
-export const TopNav = () => (
-  <header
-    sx={{
-      alignItems: "center",
-      justifyContent: "center",
-      bg: "muted",
-      display: "flex",
-      position: "sticky",
-      top: 0,
-      borderBottom: "solid",
-      borderBottomColor: "border",
-      borderBottomWidth: 1,
-      zIndex: 1,
-    }}
-  >
-    <NavLink to="/">
-      <Image src={logo} height={56} width={56} />
-    </NavLink>
-  </header>
-);
+export const TopNav = () => {
+  const [meQuery] = useMeQuery();
+
+  const { data } = meQuery;
+
+  return (
+    <header
+      sx={{
+        alignItems: "center",
+        justifyContent: "center",
+        bg: "muted",
+        display: "flex",
+        position: "sticky",
+        top: 0,
+        borderBottom: "solid",
+        borderBottomColor: "border",
+        borderBottomWidth: 1,
+        zIndex: 1,
+      }}
+    >
+      <Flex sx={{ flex: 1, mx: 4 }} />
+
+      <Flex sx={{ flex: 1, justifyContent: "center" }}>
+        <NavLink to="/">
+          <Image src={logo} height={56} width={56} />
+        </NavLink>
+      </Flex>
+
+      <Flex sx={{ flex: 1, justifyContent: "flex-end", mx: 4 }}>
+        <UserAvatar user={data?.me} size={36} />
+      </Flex>
+    </header>
+  );
+};
