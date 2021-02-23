428d80f561f8700a96cd287e9b673347ba4c1767
Ben Walker <bwalke05@gmail.com>
2021-02-22 21:50:24 -0500

    Justify at flex start

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -18,7 +18,7 @@ export const TopNav = () => {
         zIndex: 1,
       }}
     >
-      <Flex sx={{ flex: 1, mx: 4 }} />
+      <Flex sx={{ flex: 1, justifyContent: "flex-start", mx: 4 }} />
 
       <Flex sx={{ flex: 1, justifyContent: "center" }}>
         <NavLink to="/">
