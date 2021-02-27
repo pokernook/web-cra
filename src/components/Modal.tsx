1d9c745d4bc7b7971a46b28222f321dc109e1a72
Ben Walker <bwalke05@gmail.com>
2021-02-26 22:59:06 -0500

    Add opacity to modal background

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,7 @@
+/** @jsxImportSource theme-ui */
 import { FC, useRef, useState } from "react";
 import { createPortal } from "react-dom";
+import { Flex } from "theme-ui";
 
 import { useClickOutside } from "../hooks";
 
@@ -9,5 +11,26 @@ export const Modal: FC = ({ children }) => {
 
   useClickOutside(modalRef, () => setOpen(false));
 
-  return createPortal(open && <>{children}</>, document.body);
+  return createPortal(
+    open && <ModalBackground>{children}</ModalBackground>,
+    document.body
+  );
 };
+
+const ModalBackground: FC = ({ children }) => (
+  <Flex
+    sx={{
+      alignItems: "center",
+      justifyContent: "center",
+      position: "fixed",
+      top: 0,
+      left: 0,
+      bg: "rgba(0, 0, 0, 0.6)",
+      width: "100vw",
+      height: "100vh",
+      zIndex: 10,
+    }}
+  >
+    {children}
+  </Flex>
+);
