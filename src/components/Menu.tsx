76b1a501c02d2b38ee2e2b67baf2440451560c4c
Ben Walker <bwalke05@gmail.com>
2021-02-28 18:04:32 -0500

    Remove trigger prop

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,29 +1,23 @@
 /** @jsxImportSource theme-ui */
-import { FC, ReactNode, useRef, useState } from "react";
+import { FC, useRef } from "react";
 import { Card, Divider, Flex, ThemeUIStyleObject } from "theme-ui";
 
 import { useClickOutside } from "../hooks";
 
 type MenuProps = {
-  trigger: ReactNode;
+  toggle: () => void;
   sx?: ThemeUIStyleObject;
 };
 
 // TODO: Menu should close when any action is performed within the menu
-export const Menu: FC<MenuProps> = ({ children, trigger, ...props }) => {
+export const Menu: FC<MenuProps> = ({ children, toggle, ...props }) => {
   const menuRef = useRef(null);
-  const [open, setOpen] = useState(false);
-  const toggle = () => setOpen(!open);
 
-  useClickOutside(menuRef, () => setOpen(false));
+  useClickOutside(menuRef, toggle);
 
   return (
     <div ref={menuRef} sx={{ position: "relative" }} {...props}>
-      <div onClick={toggle} sx={{ ":hover": { cursor: "pointer" } }}>
-        {trigger}
-      </div>
-
-      {open && children}
+      {children}
     </div>
   );
 };
