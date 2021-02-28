933a6c3d71d2a6261307d8ba0f6c8bbfea26d876
Ben Walker <bwalke05@gmail.com>
2021-02-28 17:46:40 -0500

    Extract manu avatar

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -19,85 +19,87 @@ import {
 import { SetStatusModal } from "./SetStatusModal";
 import { UserAvatar } from "./UserAvatar";
 
-export const TopNav = () => {
-  const [modalOpen, setModalOpen] = useState(false);
+const UserMenu = () => {
   const [meQuery] = useMeQuery();
   const [, clearStatus] = useClearStatusMutation();
   const [, logOut] = useLogOutMutation();
+  const [modalOpen, setModalOpen] = useState(false);
 
   const { data } = meQuery;
 
   return (
-    <header
-      sx={{
-        alignItems: "center",
-        display: "flex",
-        bg: "background",
-        position: "sticky",
-        top: 0,
-        borderBottom: "solid",
-        borderBottomColor: "border",
-        borderBottomWidth: 1,
-        zIndex: 1,
-      }}
-    >
-      <Flex sx={{ flex: 1, justifyContent: "flex-start", mx: 4 }} />
+    <>
+      <Menu trigger={<UserAvatar user={data?.me} size={32} />}>
+        <MenuCard sx={{ right: 0, minWidth: 300 }}>
+          <MenuItem>
+            <UserAvatar user={data?.me} size={40} sx={{ mr: 2 }} />
+            <Heading as="h3">{data?.me?.username}</Heading>
+            <Heading as="h3" sx={{ color: "textMuted", fontWeight: "body" }}>
+              #{data?.me?.discriminator}
+            </Heading>
+          </MenuItem>
 
-      <Flex sx={{ flex: 1, justifyContent: "center" }}>
-        <NavLink to="/">
-          <Image src={logo} height={56} width={56} />
-        </NavLink>
-      </Flex>
+          <MenuItem>
+            <Button
+              variant="tertiary"
+              sx={{ width: "100%", textAlign: "left", bg: "background" }}
+              onClick={() => setModalOpen(true)}
+            >
+              {data?.me?.status ? (
+                <Text>
+                  {data.me.status.emoji} {data.me.status.message}
+                </Text>
+              ) : (
+                <Text color="textMuted">Update status</Text>
+              )}
+            </Button>
+          </MenuItem>
 
-      <Flex sx={{ flex: 1, justifyContent: "flex-end", mx: 4 }}>
-        <Menu trigger={<UserAvatar user={data?.me} size={32} />}>
-          <MenuCard sx={{ right: 0, minWidth: 300 }}>
-            <MenuItem>
-              <UserAvatar user={data?.me} size={40} sx={{ mr: 2 }} />
-              <Heading as="h3">{data?.me?.username}</Heading>
-              <Heading as="h3" sx={{ color: "textMuted", fontWeight: "body" }}>
-                #{data?.me?.discriminator}
-              </Heading>
-            </MenuItem>
+          {data?.me?.status && (
+            <MenuButton onClick={() => clearStatus()}>Clear status</MenuButton>
+          )}
 
-            <MenuItem>
-              <Button
-                variant="tertiary"
-                sx={{ width: "100%", textAlign: "left", bg: "background" }}
-                onClick={() => setModalOpen(true)}
-              >
-                {data?.me?.status ? (
-                  <Text>
-                    {data.me.status.emoji} {data.me.status.message}
-                  </Text>
-                ) : (
-                  <Text color="textMuted">Update status</Text>
-                )}
-              </Button>
-            </MenuItem>
+          <MenuSeparator />
 
-            {data?.me?.status && (
-              <MenuButton onClick={() => clearStatus()}>
-                Clear status
-              </MenuButton>
-            )}
+          <MenuButton>Edit profile</MenuButton>
+          <MenuButton>View profile</MenuButton>
+          <MenuButton>Settings</MenuButton>
 
-            <MenuSeparator />
+          <MenuSeparator />
 
-            <MenuButton>Edit profile</MenuButton>
-            <MenuButton>View profile</MenuButton>
-            <MenuButton>Settings</MenuButton>
-
-            <MenuSeparator />
-
-            <MenuButton onClick={() => logOut()}>
-              Log out of PokerNook
-            </MenuButton>
-          </MenuCard>
-        </Menu>
-      </Flex>
+          <MenuButton onClick={() => logOut()}>Log out of PokerNook</MenuButton>
+        </MenuCard>
+      </Menu>
 
       <SetStatusModal open={modalOpen} closeModal={() => setModalOpen(false)} />
-    </header>
+    </>
   );
 };
+
+export const TopNav = () => (
+  <Flex
+    as="header"
+    sx={{
+      alignItems: "center",
+      bg: "background",
+      position: "sticky",
+      top: 0,
+      borderBottom: "solid",
+      borderBottomColor: "border",
+      borderBottomWidth: 1,
+      zIndex: 1,
+    }}
+  >
+    <Flex sx={{ flex: 1, justifyContent: "flex-start", mx: 4 }} />
+
+    <Flex sx={{ flex: 1, justifyContent: "center" }}>
+      <NavLink to="/">
+        <Image src={logo} height={56} width={56} />
+      </NavLink>
+    </Flex>
+
+    <Flex sx={{ flex: 1, justifyContent: "flex-end", mx: 4 }}>
+      <UserMenu />
+    </Flex>
+  </Flex>
+);
