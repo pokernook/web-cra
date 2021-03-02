42f928d15d6e3fec6021e79094375d291eb6f7b9
Ben Walker <bwalke05@gmail.com>
2021-03-01 21:37:42 -0500

    Overhaul status modal, display graphql errors

--- src/components/SetStatusModal.tsx
+++ src/components/SetStatusModal.tsx
@@ -1,9 +1,10 @@
 import "emoji-mart/css/emoji-mart.css";
 
 import { BaseEmoji, Picker } from "emoji-mart";
+import { motion } from "framer-motion";
 import { FC, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Button, Field, Flex } from "theme-ui";
+import { Box, Button, Card, Close, Field, Flex, Heading, Text } from "theme-ui";
 
 import {
   SetStatusMutationVariables,
@@ -12,17 +13,16 @@ import {
   useSetStatusMutation,
 } from "../graphql";
 import { Menu } from "./Menu";
-import { Modal } from "./Modal";
+import { ModalPortal } from "./Modal";
 
 type Props = {
-  open: boolean;
   closeModal: () => void;
 };
 
-export const SetStatusModal: FC<Props> = ({ open, closeModal }) => {
+export const SetStatusModal: FC<Props> = ({ closeModal }) => {
   const [meQuery] = useMeQuery();
   const [, clearStatus] = useClearStatusMutation();
-  const [, setStatus] = useSetStatusMutation();
+  const [setStatusResult, setStatus] = useSetStatusMutation();
   const {
     control,
     handleSubmit,
@@ -32,6 +32,7 @@ export const SetStatusModal: FC<Props> = ({ open, closeModal }) => {
   const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
 
   const { data } = meQuery;
+
   const defaultEmoji = data?.me?.status?.emoji || "💬";
   const watchEmoji = watch("emoji", defaultEmoji);
 
@@ -50,65 +51,87 @@ export const SetStatusModal: FC<Props> = ({ open, closeModal }) => {
     }
   });
 
-  // TODO: Display graphql errors
   return (
-    <Modal title="Set a status" open={open} close={closeModal}>
-      <form onSubmit={handleSaveStatus}>
-        <Field
-          label={`What's happening ${data?.me?.username}?`}
-          type="text"
-          defaultValue={data?.me?.status?.message || ""}
-          name="message"
-          ref={register({ required: true })}
-          spellCheck
-          pl={4}
+    <ModalPortal close={closeModal} fadeBackground>
+      <Card variant="modal">
+        <Close
+          onClick={closeModal}
+          sx={{ position: "absolute", top: 10, right: 10 }}
         />
+        <Heading>Set a status</Heading>
 
-        <Button
-          variant="unstyled"
-          type="button"
-          onClick={toggleEmojiPicker}
-          sx={{ position: "absolute", p: 1, top: 82 }}
-        >
-          {watchEmoji}
-        </Button>
+        <Box as="form" onSubmit={handleSaveStatus} mt={3}>
+          <Field
+            label={`What's happening ${data?.me?.username}?`}
+            type="text"
+            defaultValue={data?.me?.status?.message || ""}
+            name="message"
+            ref={register({ required: true })}
+            spellCheck
+            pl={4}
+          />
 
-        <Controller
-          name="emoji"
-          control={control}
-          defaultValue={defaultEmoji}
-          render={(props) => (
-            <Menu close={closeEmojiPicker} open={emojiPickerOpen}>
-              <Picker
-                title="Pick an emoji"
-                emoji="point_up"
-                native
-                theme="dark"
-                style={{ position: "absolute" }}
-                onSelect={(emoji: BaseEmoji) => {
-                  props.onChange(emoji.native);
-                  closeEmojiPicker();
-                }}
-              />
-            </Menu>
-          )}
-        />
-
-        <Flex sx={{ float: "right", mt: 3 }}>
           <Button
-            variant="tertiary"
+            variant="unstyled"
             type="button"
-            mr={2}
-            onClick={handleClearStatus}
+            onClick={toggleEmojiPicker}
+            sx={{ position: "absolute", p: 1, top: 82 }}
           >
-            Clear status
+            {watchEmoji}
           </Button>
 
-          <Button variant="secondary" type="submit">
-            Save status
-          </Button>
-        </Flex>
-      </form>
-    </Modal>
+          <Controller
+            name="emoji"
+            control={control}
+            defaultValue={defaultEmoji}
+            render={(props) =>
+              emojiPickerOpen ? (
+                <Menu close={closeEmojiPicker}>
+                  <Picker
+                    title="Pick an emoji"
+                    emoji="point_up"
+                    native
+                    theme="dark"
+                    style={{ position: "absolute" }}
+                    onSelect={(emoji: BaseEmoji) => {
+                      props.onChange(emoji.native);
+                      closeEmojiPicker();
+                    }}
+                  />
+                </Menu>
+              ) : (
+                <></>
+              )
+            }
+          />
+
+          <Box mt={1} mb={3}>
+            {setStatusResult.error && (
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                <Text variant="danger">
+                  {setStatusResult.error.graphQLErrors[0]?.message ||
+                    setStatusResult.error.networkError?.message}
+                </Text>
+              </motion.div>
+            )}
+          </Box>
+
+          <Flex sx={{ float: "right" }}>
+            <Button
+              variant="tertiary"
+              type="button"
+              mr={2}
+              onClick={handleClearStatus}
+            >
+              Clear status
+            </Button>
+
+            <Button variant="secondary" type="submit">
+              Save status
+            </Button>
+          </Flex>
+        </Box>
+      </Card>
+    </ModalPortal>
   );
 };
