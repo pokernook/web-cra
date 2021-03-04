68676ef1215364d0399685c9010b298b53441174
Ben Walker <bwalke05@gmail.com>
2021-03-03 21:47:21 -0500

    Reorganize props; use width and height props directly

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -6,12 +6,12 @@ import { generateAvatarSvg } from "../util/generate-avatar";
 
 type Props = {
   size: number;
-  sx?: ThemeUIStyleObject;
   user?: UserFieldsFragment | null;
+  sx?: ThemeUIStyleObject;
 };
 
 export const UserAvatar: FC<Props> = ({ size, user, ...props }) => {
   const src = useMemo(() => generateAvatarSvg(user?.id || ""), [user?.id]);
 
-  return <Avatar src={src} sx={{ width: size, height: size }} {...props} />;
+  return <Avatar src={src} width={size} height={size} {...props} />;
 };
