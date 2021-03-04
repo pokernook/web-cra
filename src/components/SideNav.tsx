790dae160a5c4e9e76c8c2f91034ecc9c32f3a88
Ben Walker <bwalke05@gmail.com>
2021-03-03 21:35:38 -0500

    Move sidenav to separate component

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,49 @@
+/** @jsxImportSource theme-ui */
+import { NavLink, NavLinkProps } from "react-router-dom";
+import { Box, Divider, Flex, Heading } from "theme-ui";
+
+import { useMeQuery } from "../graphql";
+import { UserAvatar } from "./UserAvatar";
+
+const sideNavRoutes: NavLinkProps[] = [
+  { to: "/", exact: true, children: "Home" },
+  { to: "/settings", exact: false, children: "Settings" },
+];
+
+export const SideNav = () => {
+  const [meQuery] = useMeQuery();
+
+  const { data } = meQuery;
+
+  return (
+    <aside
+      sx={{
+        borderRight: "solid",
+        borderRightColor: "border",
+        borderRightWidth: 1,
+      }}
+    >
+      <Box sx={{ position: "sticky", top: 5 }}>
+        <Flex sx={{ alignItems: "center", mx: 3 }}>
+          <UserAvatar user={data?.me} size={48} sx={{ mr: 2 }} />
+          <Heading as="h3">{data?.me?.username}</Heading>
+          <Heading as="h3" sx={{ color: "textMuted", fontWeight: "body" }}>
+            #{data?.me?.discriminator}
+          </Heading>
+        </Flex>
+
+        <Divider my={3} />
+
+        <Box as="nav" mx={3}>
+          {sideNavRoutes.map((route, index) => (
+            <NavLink
+              key={index}
+              {...route}
+              sx={{ variant: "links.nav", my: 1, display: "inherit" }}
+            />
+          ))}
+        </Box>
+      </Box>
+    </aside>
+  );
+};
