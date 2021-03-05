5f4f377d4ad15b90473a410e70181567e6c0a5db
Ben Walker <bwalke05@gmail.com>
2021-03-04 21:21:00 -0500

    Move cache updates and resolvers to a separate file

---
+++ src/graphql/cache.ts
@@ -0,0 +1,53 @@
+import { Data, ResolverConfig, UpdatesConfig } from "@urql/exchange-graphcache";
+
+import * as graphql from "./types";
+
+export const resolvers: ResolverConfig = {
+  User: {
+    discriminator: (parent) =>
+      parent.discriminator?.toString().padStart(4, "0"),
+  },
+  UserStatus: {
+    emoji: (parent) => parent.emoji ?? "💬",
+  },
+};
+
+export const updates: Partial<UpdatesConfig> = {
+  Mutation: {
+    userLogIn: (result, _args, cache) => {
+      cache.updateQuery({ query: graphql.MeDocument }, (data) => {
+        const castData = data as graphql.MeQuery;
+        const castResult = result as graphql.LogInMutation;
+        castData.me = castResult.userLogIn?.user;
+        return castData as Data;
+      });
+    },
+
+    userSignUp: (result, _args, cache) => {
+      cache.updateQuery({ query: graphql.MeDocument }, (data) => {
+        const castData = data as graphql.MeQuery;
+        const castResult = result as graphql.SignUpMutation;
+        castData.me = castResult.userSignUp?.user;
+        return castData as Data;
+      });
+    },
+
+    userLogOut: (_result, _args, cache) => {
+      cache.invalidate("Query", "me");
+    },
+
+    userDeleteAccount: (_result, _args, cache) => {
+      cache.invalidate("Query", "me");
+    },
+
+    userClearStatus: (_result, _args, cache) => {
+      cache.updateQuery({ query: graphql.MeDocument }, (data) => {
+        const castData = data as graphql.MeQuery;
+        if (castData.me) {
+          castData.me.status = null;
+        }
+        return castData as Data;
+      });
+    },
+  },
+};
