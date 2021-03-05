3094bc7253ab70c916b1fa11c90d372717522b2e
Ben Walker <bwalke05@gmail.com>
2021-03-04 21:09:24 -0500

    Break out layout component

--- src/PrivateApp.tsx
+++ src/PrivateApp.tsx
@@ -1,54 +1,20 @@
-/** @jsxImportSource theme-ui */
-import { FC } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import { Grid } from "theme-ui";
 
-import { SideNav } from "./components/SideNav";
-import { TopNav } from "./components/TopNav";
+import { PrivateLayout } from "./layouts/Private";
 import { Settings } from "./pages/Settings";
 
-const Main: FC = ({ children }) => (
-  <main
-    sx={{
-      display: "inherit",
-      minHeight: "inherit",
-      minWidth: 320,
-      width: "100%",
-    }}
-  >
-    {children}
-  </main>
-);
-
-const PrivateAppLayout: FC = ({ children }) => (
-  // TODO: Fix gap at bottom of page on Safari
-  <Grid
-    columns={["auto"]}
-    gap="0px"
-    sx={{
-      minHeight: "100vh",
-      width: "100vw",
-      gridTemplateRows: "50px auto",
-    }}
-  >
-    <TopNav />
-    <Grid columns={["280px 1fr"]} gap="0px">
-      <SideNav />
-      <Main>{children}</Main>
-    </Grid>
-  </Grid>
-);
-
 export const PrivateApp = () => (
-  <PrivateAppLayout>
+  <PrivateLayout>
     <Switch>
       <Route exact path="/"></Route>
+
       <Route path="/settings">
         <Settings />
       </Route>
+
       <Route>
         <Redirect to="/" />
       </Route>
     </Switch>
-  </PrivateAppLayout>
+  </PrivateLayout>
 );
