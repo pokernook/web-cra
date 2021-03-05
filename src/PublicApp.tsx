cb6b986bcc26a5d93233305644d1632333092f4b
Ben Walker <bwalke05@gmail.com>
2021-03-04 21:13:26 -0500

    Move layout to separate file

--- src/PublicApp.tsx
+++ src/PublicApp.tsx
@@ -1,30 +1,23 @@
-import { FC } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import { Container, Image } from "theme-ui";
 
-import logo from "./assets/logo.svg";
+import { PublicLayout } from "./layouts/Public";
 import { LogIn } from "./pages/LogIn";
 import { SignUp } from "./pages/SignUp";
 
-const PublicAppLayout: FC = ({ children }) => (
-  <Container sx={{ maxWidth: 325, pt: 20, textAlign: "center" }}>
-    <Image height={128} width={128} src={logo} mb={2} />
-    {children}
-  </Container>
-);
-
 export const PublicApp = () => (
-  <PublicAppLayout>
+  <PublicLayout>
     <Switch>
       <Route exact path="/logIn">
         <LogIn />
       </Route>
+
       <Route exact path="/">
         <SignUp />
       </Route>
+
       <Route>
         <Redirect to="/" />
       </Route>
     </Switch>
-  </PublicAppLayout>
+  </PublicLayout>
 );
