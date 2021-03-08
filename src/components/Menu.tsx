07714b1391d815d34c30c8d313e7283607be7a27
Ben Walker <bwalke05@gmail.com>
2021-03-07 20:20:52 -0500

    Add menu item component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Button, ButtonProps, Card, CardProps, Divider } from "theme-ui";
+import { Button, ButtonProps, Card, CardProps, Divider, Flex } from "theme-ui";
 
 export const MenuCard: FC<CardProps> = ({ children, ...props }) => (
   <Card variant="menu" {...props}>
@@ -13,4 +13,8 @@ export const MenuButton: FC<ButtonProps> = ({ children, ...props }) => (
   <Button variant="menu" {...props}>
     {children}
   </Button>
+);
+
+export const MenuItem: FC = ({ children }) => (
+  <Flex sx={{ px: 3, py: 1, alignItems: "center" }}>{children}</Flex>
 );
