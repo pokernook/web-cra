4b7e9731d21e8a3114de91dcdd3e274fe1ee500a
Ben Walker <bwalke05@gmail.com>
2021-03-15 21:28:04 -0400

    Stub out friends page

--- src/PrivateApp.tsx
+++ src/PrivateApp.tsx
@@ -1,13 +1,16 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import { PrivateLayout } from "./layouts/Private";
+import { Friends } from "./pages/Friends";
 
 export const PrivateApp = () => (
   <PrivateLayout>
     <Switch>
       <Route exact path="/"></Route>
 
-      <Route exact path="/friends"></Route>
+      <Route exact path="/friends">
+        <Friends />
+      </Route>
 
       <Route>
         <Redirect to="/" />
